fa454a45f58bfc3537e43240609c273a2684d84f
ANKIT KUMAR <ankitabcd1718@gmail.com>
2025-05-21 15:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -262,7 +262,12 @@ def check_password():
         'feedback': feedback
     })
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
     # app.run(debug=Config.DEBUG) 
